943391bc4537ed137f05d603f5514fb0225302ed
vibhanshuverma02 <vibhanshuverma.dpsr@gmail.com>
2025-03-27 14:34:28 +0530

    5

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   experimental: {
-    turbo: {
-      enabled: false,    // ✅ Correct syntax to disable Turbopack
-    }
+    serverActions: true,
+  },
+   /**
+   * @param {import('webpack').Configuration} config - Webpack configuration
+   * @returns {import('webpack').Configuration}
+   */
+  // @ts-ignore
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      type: "javascript/auto",  // Handle ESM modules
+    });
+
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+    };
+    return config;
   }
 };
 
