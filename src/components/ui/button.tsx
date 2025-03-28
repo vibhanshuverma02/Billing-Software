8a3ad70b8ea1f7c71931f26c6e3dc134a154126e
vibhanshuverma02 <vibhanshuverma.dpsr@gmail.com>
2025-03-28 14:09:48 +0530

    fixed 3

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -50,7 +50,7 @@ function Button({
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
+       {...(props as React.HTMLAttributes<HTMLElement>)} 
     />
   )
 }
