bf6e6a34c5222459183466cdecdf8ad7aac12a94
vibhanshuverma02 <vibhanshuverma.dpsr@gmail.com>
2026-05-08 12:34:43 +0530

    package

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
-import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig = {
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({
   experimental: {
     // ✅ Enable ESM support
-  }
-};
+  },
+});
 
 export default nextConfig;
